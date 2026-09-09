bba9029c122b0efd201aa31aa6efaf1cf38ba0c5
3dbatataro4-dotcom <3dbatataro4@gmail.com>
2026-09-09 15:21:56 +0800

    更新 service-worker.js via EZPage

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "oc-profile-app-v3";
+const APP_SHELL = ["./", "./index.html", "./style.css", "./data.js", "./app.js", "./manifest.webmanifest", "./app-icon-192.png", "./app-icon-512.png"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET" || new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request, { ignoreSearch:true }).then(hit => hit || (event.request.mode === "navigate" ? caches.match("./index.html") : Response.error()))));
+});
