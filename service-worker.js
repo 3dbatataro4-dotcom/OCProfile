378771a23780f123573f16a0591f9763519b905d
3dbatataro4-dotcom <3dbatataro4@gmail.com>
2026-09-12 21:59:08 +0800

    更新 service-worker.js via EZPage

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "oc-profile-app-v21-4-cloud-notes-forum-drawer";
+const CACHE_NAME = "oc-profile-app-v21-5-scroll-safe-mobile-layout";
 const APP_SHELL = ["./", "./index.html", "./style.css", "./data.js", "./app.js", "./forum.css", "./forum-chat.css", "./forum-chat-core.js", "./forum-chat.js", "./forum-core.js", "./forum.js", "./cloud-sync-core.js", "./cloud-sync.js", "./cloud-sync.css", "./manifest.webmanifest", "./app-icon-192.png", "./app-icon-512.png"];
 
 self.addEventListener("install", event => {
