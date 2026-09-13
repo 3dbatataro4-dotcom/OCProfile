9dcae52d0d183ad2576165ed54b38fbd753f6ae2
3dbatataro4-dotcom <3dbatataro4@gmail.com>
2026-09-14 05:37:24 +0800

    更新 service-worker.js via EZPage

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "oc-profile-app-v21-44-vn-dialogue-anchor";
+const CACHE_NAME = "oc-profile-app-v21-45-vn-visible-dialogue";
 const APP_SHELL = ["./", "./index.html", "./style.css", "./data.js", "./app.js", "./forum.css", "./forum-chat.css", "./forum-chat-core.js", "./forum-chat.js", "./forum-core.js", "./forum.js", "./cloud-sync-core.js", "./cloud-sync.js", "./cloud-sync.css", "./manifest.webmanifest", "./app-icon-192.png", "./app-icon-512.png"];
 
 self.addEventListener("install", event => {
